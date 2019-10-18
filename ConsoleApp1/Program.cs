8182b8cf5f275d6b99cba2fe27920cebf05dce54
Enver Kamesnicanin <omer.teamviewer@hotmail.com>
2019-10-18 16:21:53 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karakter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            Console.WriteLine("Uneti opseg Karaktera ");
+
+            Console.Write("od: ");
+
+            char Karakter = (char)(int.Parse(Console.ReadLine()));
+
+            Console.Write("do: ");
+
+            char kaarakter = (char)(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Koliko Karaktera po redu? ");
+
+            int karakter =(int.Parse(Console.ReadLine()));
+
+            while (Karakter <= kaarakter)
+            {
+                Console.Write(Karakter);
+                if ((Karakter - 126 ) % karakter == 0 )
+                {
+                    Console.WriteLine();
+                }
+                
+                Karakter++;
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
